e2857c868aa6787ff7bf47fcb5b14e93f9c6ac39
Vikrant Kumar <axelice89@gmail.com>
2022-03-25 10:52:51 +0530

    Update ios_config.js

--- ios_config.js
+++ ios_config.js
@@ -1,9 +1,9 @@
 config.iosNotification = {
     passenger: {
-      keyId: "D9DM65LMKF",
-      teamId: "952ZU65CH6",
-      topicProductionFalse: 'com.arya.WatchOutPassenger.local',
-      topicProductionTrue: 'com.arya.WatchOutPassenger',
-      topicStagingTrue: 'com.arya.WatchoutPassenger.stage',
+      keyId: "",
+      teamId: "",
+      topicProductionFalse: ''
+      topicProductionTrue: '',
+      topicStagingTrue: '',
     }
   }
